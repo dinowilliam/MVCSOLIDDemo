e2b47726082c5aba9cbce47de1c8e05351161641
William Ferreira de Paula <dinowilliam@gmail.com>
2018-05-14 13:56:56 -0300

    Add validate in BaseDomainModel

---
+++ MVCSOLIDDemo.Domain/Models/Contracts/BaseDomainModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace MVCSOLIDDemo.Domain.Models
+{
+    using FluentValidator.Validation;
+    using MVCSOLIDDemo.Domain.Models.Contracts;
+    using MVCSOLIDDemo.Utils.Notification;
+    using MVCSOLIDDemo.Utils.Notification.Contracts;
+    using System.Collections.Generic;
+
+    public abstract class BaseDomainModel : IBaseDomainModel
+    {
+
+        public Guid Id { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? DisabledAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public Boolean IsEnabled
+        {
+
+            get
+            {
+
+                if(CreatedAt.HasValue && DisabledAt.HasValue == false && DeletedAt.HasValue == false)
+                {
+
+                    return true;
+                }
+                else
+                {
+
+                    return false;
+                }
+
+            }
+        }
+
+        public Boolean IsDeleted
+        {
+
+            get
+            {
+
+                if(DeletedAt.HasValue == false)
+                {
+
+                    return true;
+                }
+                else
+                {
+
+                    return false;
+                }
+            }
+
+        }
+
+        public INotification Notification { get; set; }
+
+        public bool IsValid { get; }
+
+        private bool Validate(Type valContract) {
+
+            IContract validationContract = (IContract) Activator.CreateInstance(valContract, this);
+
+            if(!validationContract.Contract.Valid)
+            {
+                var listNotification = (IList<INotificationItem>) new List<NotificationItem>();
+                var notification = new Notification(listNotification);
+
+                foreach(var item in validationContract.Contract.Notifications)
+                {
+
+                    var newNotification = new NotificationItem()
+                    {
+                        Message = item.Message,
+                        Description = item.Property
+                    };
+
+                    notification.Notifications.Add(newNotification);
+
+                }
+            }
+
+            return validationContract.Contract.Valid;
+
+        }
+
+    }
+
+}
---
+++ MVCSOLIDDemo.Domain/Models/Validation/UserContract .cs
@@ -0,0 +1,20 @@
+﻿using FluentValidator.Validation;
+
+namespace MVCSOLIDDemo.Domain.Models.Validation
+{
+    public class UserContract : IContract {
+
+        public ValidationContract Contract { get; }
+
+        public UserContract(User user) {
+
+            Contract = new ValidationContract()
+                .Requires()
+                .HasMinLen(user.Name, 3, "Name", "First Name should have at least 3 chars")
+                .HasMaxLen(user.Name, 30, "Name", "First Name should not have more than 3 chars")
+                .HasMinLen(user.Surname, 3, "Surname", "Last Name should have at least 3 chars")
+                .HasMaxLen(user.Surname, 50, "Surname", "Last Name should not have more than 3 chars");
+        }
+
+    }
+}
