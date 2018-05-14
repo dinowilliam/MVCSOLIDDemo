e6d48fcb1d59d161de4f973997bf3cd48f34c693
William Ferreira de Paula <dinowilliam@gmail.com>
2018-05-14 15:21:13 -0300

    Add Domain User Age Test

--- MVCSOLIDDemo.Tests/Domain/DomainTests.cs
+++ MVCSOLIDDemo.Tests/Domain/DomainTests.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using FakeItEasy;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 namespace MVCSOLIDDemo.Domain.Tests {
+
+    using MVCSOLIDDemo.Domain.Models;
+    using MVCSOLIDDemo.Domain.Models.Contracts;
+
     [TestClass]
     public class DomainTests {
 
         [TestMethod]
-        public void TestMethod1() {
+        public void TestDomainUserPropertieAge() {
+
+            var listAddress = A.Fake<List<IAddress>>(); 
+
+            var user = new User("Usuario", "", "", "", "", DateTime.Now.AddYears(-37), listAddress);
+
+            Assert.AreEqual(user.Age, 37);
+
         }
+
 
     }
 }
