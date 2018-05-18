031546019d380d08d03e7c15fd5ca438629fe404
William Ferreira de Paula <dinowilliam@gmail.com>
2018-05-18 18:17:37 -0300

    Add test project to Utils.StringHelper

---
+++ MVCSOLIDDemo.Tests/Solution/CrossCutting/Utils/UtilsTestsStringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MVCSOLIDDemo.Tests.CrossCutting.Utils {
+    
+    using MVCSOLIDDemo.Utils.Helpers.Primitives;
+
+    [TestClass]
+    public class UtilsTestsStringHelper
+    {
+
+        [TestMethod]
+        public void TestUtilsStringHelperHashMD5() {
+            
+            var stringMD5 = StringHelper.HashMD5("AAAAAAAAA");
+
+            Assert.AreEqual(stringMD5, "6c9395cacd317eed2777f669103b7181");
+        }
+
+    }
+}
