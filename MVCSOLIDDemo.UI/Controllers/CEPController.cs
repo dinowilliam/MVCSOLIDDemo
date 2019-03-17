3bed4b99bc0d3ee05cc061f4b3c8f83b463a2fcd
dinowillaim@gmail.com <dinowilliam>
2019-03-17 20:54:20 -0300

    Inclusão de menu e novas pagina inicial do boostrap

---
+++ MVCSOLIDDemo.UI/Controllers/CEPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSOLIDDemo.UI.Controllers
+{
+    public class CEPController : Controller
+    {
+        // GET: CEP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
