83f42e78101eeffc451f42f55120d4731ba66479
William Ferreira de Paula <dinowilliam@outlook.com>
2019-04-12 17:54:33 -0300

    Alterações ao carregar a tela de CEP

---
+++ MVCSOLIDDemo.UI/Scripts/CEP/CEP.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    $('#txtCEP').inputmask('99999-999');
+
+    $('#btnPesquisar').click(function (event) {
+
+        var jsonViaCEP = 'https://viacep.com.br/ws/' + $('#txtCEP').val() + '/json';
+
+        $.getJSON(jsonViaCEP, function (data) {
+
+            $("#lblCEP").text(data['cep']);
+            $("#lblLogradouro").text(data['logradouro']);
+            $("#lblComplemento").text(data['complemento']);
+            $("#lblBairro").text(data['bairro']);
+            $("#lblLocalidade").text(data['localidade']);
+            $("#lblUF").text(data['uf']);
+            $("#lblUnidade").text(data['unidade']);
+            $("#lblIBGE").text(data['ibge']);
+            $("#lblGIA").text(data['gia']);
+
+        });
+
+    });
+
+});
