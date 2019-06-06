6fe90e433c6fc85cfbe2aedbe5362748891e760c
William Ferreira de Paula <dinowilliam@outlook.com>
2019-06-06 16:49:35 -0300

    Alterações para utilização do Vue

--- MVCSOLIDDemo.UI/Scripts/CEPVue/CEPVue.js
+++ MVCSOLIDDemo.UI/Scripts/CEPVue/CEPVue.js
@@ -1,6 +1,31 @@
 ﻿var cepVue = new Vue({
     el: '#appCep',
     data: {
-        message: 'Hello Vue!'
+        cep:'',
+        logradouro:'',
+        complemento:'',
+        bairro:'',
+        localidade:'',
+        uf:'',
+        unidade:'',
+        ibge:'',
+        gia:''
+    },
+    methods: {
+        consultarCep: function () {
+            var jsonViaCEP = 'https://viacep.com.br/ws/' + $('#txtCEP').val() + '/json';
+
+            http.request({
+                url: jsonViaCEP,
+                method: "GET",
+                headers: { "Content-Type": "application/json" }               
+            }).then(response => {
+               data = response.content.toJSON();
+            }, error => {
+                console.error(error);
+            });
+            
+        }
     }
+
 })
