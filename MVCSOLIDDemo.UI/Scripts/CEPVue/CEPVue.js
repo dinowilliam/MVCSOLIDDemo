fcb4610267eeb4b07beca7ef7badaa2b1c16991a
William Ferreira de Paula <dinowilliam@outlook.com>
2019-06-07 11:09:45 -0300

    Alterações para utilziar Axios

--- MVCSOLIDDemo.UI/Scripts/CEPVue/CEPVue.js
+++ MVCSOLIDDemo.UI/Scripts/CEPVue/CEPVue.js
@@ -1,31 +1,25 @@
 ﻿var cepVue = new Vue({
     el: '#appCep',
     data: {
-        cep:'',
-        logradouro:'',
-        complemento:'',
-        bairro:'',
-        localidade:'',
-        uf:'',
-        unidade:'',
-        ibge:'',
-        gia:''
+        cep: '',
+        logradouro: '',
+        complemento: '',
+        bairro: '',
+        localidade: '',
+        uf: '',
+        unidade: '',
+        ibge: '',
+        gia: ''
     },
     methods: {
         consultarCep: function () {
             var jsonViaCEP = 'https://viacep.com.br/ws/' + $('#txtCEP').val() + '/json';
 
-            http.request({
-                url: jsonViaCEP,
-                method: "GET",
-                headers: { "Content-Type": "application/json" }               
-            }).then(response => {
-               data = response.content.toJSON();
-            }, error => {
-                console.error(error);
-            });
-            
+            axios
+                .get(jsonViaCEP)
+                .then(response => (this.data = response));                          
+
         }
     }
 
-})
+});
